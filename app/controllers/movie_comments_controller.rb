cd6e6bac24199d7250f404e9d37baf8680fe4c14
dango-1104 <570401mn@gmail.com>
2021-07-11 18:03:36 +0000

    third commit

---
+++ app/controllers/movie_comments_controller.rb
@@ -0,0 +1,7 @@
+class MovieCommentsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'homes#top'
+  get '/home/about' => 'homes#about'
+  resources :movies do
+    resources :movie_comments, only: [:create, :destroy]
+     resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
