ce7446a538d468294106a23e369ccb82dae4062f
dango-1104 <570401mn@gmail.com>
2021-07-17 17:14:46 +0000

    userのshow,edit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '642a3e99bddc7fd0b78ab1ade7c0345bc42fd5ab3f4d004a5dd82913f90ec7c187c28fd3a36cace46db164d668ae870a8e75d528c65813c14bba294758a1c3c4'
